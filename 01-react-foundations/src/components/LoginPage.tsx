21d20fc07ea331f5f2b33f6a9667c8d3f477f82d
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2024-04-26 21:50:21 +0100

    Login & Logout

--- 01-react-foundations/src/components/LoginPage.tsx
+++ 01-react-foundations/src/components/LoginPage.tsx
@@ -1,14 +1,42 @@
-import {useAuthStore} from "../store/auth.store.ts";
+import {useAuthStore} from "../store";
+import {useEffect} from "react";
 
-export const LoginPage = ()=> {
+export const LoginPage = () => {
 
     const authStatus = useAuthStore(state => state.status)
+    const user = useAuthStore(state => state.user)
+    const login = useAuthStore(state => state.login);
+    const logout = useAuthStore(state => state.logout);
+
+    useEffect(() => {
+        setTimeout(() => {
+            logout();
+        }, 1500);
+    }, [])
+
+    if (authStatus === 'checking') {
+        return <p>Loading...</p>;
+    }
 
     return (
         <>
             <h3>Login Page</h3>
 
-            {authStatus}
+            {
+                (authStatus === 'authenticated')
+                    ? <div>Authenticated as: {JSON.stringify(user, null, 2)}</div>
+                    : <div>Unauthenticated</div>
+            }
+
+            {
+                (authStatus === 'authenticated')
+                    ? (
+                        <button onClick={logout}>Logout</button>
+                    )
+                    : (
+                        <button onClick={() => login('javier.rojas.blum@gmail.com', '123456')}>Login</button>
+                    )
+            }
         </>
     );
 }
