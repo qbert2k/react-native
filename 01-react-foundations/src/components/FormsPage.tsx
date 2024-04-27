6436ab54cdba5520e2cff9e1df23977bb243b071
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2024-04-27 11:29:36 +0100

    Forms

--- 01-react-foundations/src/components/FormsPage.tsx
+++ 01-react-foundations/src/components/FormsPage.tsx
@@ -7,25 +7,36 @@ type FormInput = {
 
 export const FormsPage = () => {
 
-    const {register} = useForm<FormInput>({
+    const {
+        register,
+        handleSubmit,
+        formState,
+        watch
+    } = useForm<FormInput>({
         defaultValues: {
-            email: "javier.rojas.blum@gmail.com",
-            password: "123456"
+            email: "",
+            password: ""
         },
     });
+
+    const onSubmit = (myDOrm: FormInput) => {
+        console.log({myDOrm});
+    }
+
+    console.log(watch('email'));
 
     return (
         <>
             <h3>Forms</h3>
 
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div style={{
                     display: "flex",
                     flexDirection: 'column'
                 }}>
                     <input type="text"
                            placeholder="Email"
-                           {...register('email')} />
+                           {...register('email', {required: true})} />
 
                     <input type="text"
                            placeholder="Password"
@@ -34,6 +45,10 @@ export const FormsPage = () => {
                     <button type="submit">Login</button>
                 </div>
             </form>
+
+            <pre>
+                {JSON.stringify(formState, null, 2)}
+            </pre>
         </>
     );
 }
